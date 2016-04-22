222b4e9f8f36bf4805a8c3687abeb5169243f478
tduc9194 <tduc9194@gmail.com>
2016-04-22 18:31:47 +0700

    TrieuChung

--- src/action/QuanLy_DanhSachTrieuChungAction.java
+++ src/action/QuanLy_DanhSachTrieuChungAction.java
@@ -4,9 +4,14 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.struts.action.Action;
+import org.apache.struts.action.ActionErrors;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
+import org.apache.struts.action.ActionMessage;
+
+import form.TrieuChungForm;
+import model.bo.TrieuChungBO;
 
 public class QuanLy_DanhSachTrieuChungAction extends Action {
 
@@ -14,10 +19,36 @@ public class QuanLy_DanhSachTrieuChungAction extends Action {
 	public ActionForward execute(ActionMapping mapping, ActionForm form,
 			HttpServletRequest request, HttpServletResponse response)
 			throws Exception {
+		TrieuChungForm trieuChungForm = (TrieuChungForm)form;
+		TrieuChungBO trieuChungBO = new TrieuChungBO();
 		
 		
+		if(request.getParameter("maTrieuChungXoa")!=null){
+			String maTrieuChung = request.getParameter("maTrieuChungXoa");
+			trieuChungBO.xoaTrieuChung(maTrieuChung);
+		}
 		
-		return mapping.findForward("thanhCong");
+		if("Add".equals(trieuChungForm.getSubmit())){
+			String tenTrieuChung = new String(trieuChungForm.getTenTrieuChung().getBytes("ISO-8859-1"),"UTF-8");
+			
+        	ActionErrors actionErrors  = new ActionErrors();
+
+			if("".equals(tenTrieuChung)||tenTrieuChung==null){
+				actionErrors.add("tenTrieuChungError",new ActionMessage("error.notNull"));
+			}
+			else if(trieuChungBO.checkExist(tenTrieuChung)==true){
+				System.out.println("bitrung");
+				actionErrors.add("tenTrieuChungError",new ActionMessage("error.exists"));
+			}
+			saveErrors(request, actionErrors);
+			if(actionErrors.size()==0){
+        		trieuChungBO.themTrieuChung(tenTrieuChung);
+        	}
+		}
+		
+		trieuChungForm.setListTrieuChung(trieuChungBO.getListTrieuChung());
+		
+		return mapping.findForward("danhSachTrieuChung");
 		
 	}
 }
